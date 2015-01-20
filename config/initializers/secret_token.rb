2f81d16e9922a091cfa104f3696d86e1504a7cce
Jyrmi <jeremyschoi@gmail.com>
2015-01-20 00:15:42 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jeremysc::Application.config.secret_key_base = 'f98a1cd70bd171f0ed0feccb5f9a2fd8ebcb311ef1225ef6c9734a6db05c0043f896ad7398daad13c54f93268d35784628648f5675069b2f6fe6866b39d59156'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jeremysc::Application.config.session_store :cookie_store, key: '_Jeremysc_session'
